15316fad4de867f50420d125dde91be4e4f063c5
RafaelPires2 <rafaelbrpires@gmail.com>
2023-03-29 16:21:37 -0300

    docs: structure eslint and prettier

--- src/components/login-form/index.tsx
+++ src/components/login-form/index.tsx
@@ -1,13 +1,13 @@
-import styles from "./styles.module.css";
-import React, { useState } from "react";
+import styles from './styles.module.css';
+import React, { useState } from 'react';
 
 export function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [emailIsValid, setEmailIsValid] = useState(false);
   const [passwordIsValid, setPasswordIsValid] = useState(false);
-  const [errorMessageEmail, setErrorMessageEmail] = useState("");
-  const [errorMessagePassword, setErrorMessagePassword] = useState("");
+  const [errorMessageEmail, setErrorMessageEmail] = useState('');
+  const [errorMessagePassword, setErrorMessagePassword] = useState('');
 
   function handleChangeEmail(event: any) {
     const newEmail = event.target.value;
@@ -21,28 +21,25 @@ export function LoginForm() {
     const newPassword = event.target.value;
     setPassword(newPassword);
 
-    const passwordRegex =
-      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+=[\]{}|;':",.<>/?]{7,}$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+=[\]{}|;':",.<>/?]{7,}$/;
     setPasswordIsValid(passwordRegex.test(newPassword));
   }
   function clearInputs() {
-    setEmail("");
-    setPassword("");
-    setErrorMessageEmail("");
-    setErrorMessagePassword("");
+    setEmail('');
+    setPassword('');
+    setErrorMessageEmail('');
+    setErrorMessagePassword('');
   }
 
   function handleSubmit(event: any) {
     event.preventDefault();
     if (emailIsValid && passwordIsValid) {
-      console.log("entrando...");
+      console.log('entrando...');
       clearInputs();
     } else if (!emailIsValid) {
-      setErrorMessageEmail("O email deve conter email@email.com");
+      setErrorMessageEmail('O email deve conter email@email.com');
     } else if (!passwordIsValid) {
-      setErrorMessagePassword(
-        "A senha deve conter no mínimo 7 caracteres 1 letra e 1 número"
-      );
+      setErrorMessagePassword('A senha deve conter no mínimo 7 caracteres 1 letra e 1 número');
     } else {
       console.error(ErrorEvent);
     }
@@ -53,28 +50,14 @@ export function LoginForm() {
   return (
     <>
       <form className={styles.formLogin} onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="text"
-          placeholder="Digite seu email"
-          value={email}
-          onChange={handleChangeEmail}
-          required
-        />
+        <label htmlFor='email'>Email</label>
+        <input name='email' type='text' placeholder='Digite seu email' value={email} onChange={handleChangeEmail} required />
         <p>{errorMessageEmail}</p>
 
-        <label htmlFor="password">Senha</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Digite sua senha"
-          value={password}
-          onChange={handleChangePassword}
-          required
-        />
+        <label htmlFor='password'>Senha</label>
+        <input name='password' type='password' placeholder='Digite sua senha' value={password} onChange={handleChangePassword} required />
         <p>{errorMessagePassword}</p>
-        <button type="submit">Entrar</button>
+        <button type='submit'>Entrar</button>
       </form>
     </>
   );
