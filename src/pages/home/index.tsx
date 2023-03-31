ee8ac1e615856134861ccf513de7653401e20bce
RafaelPires2 <rafaelbrpires@gmail.com>
2023-03-31 11:41:16 -0300

    feature: pages dashboard, header, home, login

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../header';
+
+export function Home() {
+  return (
+    <>
+      <Header />
+      <h1>Página Home</h1>
+      <p>Seja bem vindo ao Instituto Taqtile</p>
+    </>
+  );
+}
